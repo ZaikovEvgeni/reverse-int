ebffa5677f14fd76e6fac2a176b5cad9e45735a1
Zaikou Eugene <evgeniikas9@gmail.com>
2021-08-11 15:17:38 +0300

    feat: did this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let p = Math.abs(n);
+
+  let str = String(p);
+  console.log('str '  + str)
+  let newNum = '';
+  for( let i = str.length-1; i >= 0; i--) {
+   newNum += str[i];
+  }
+  console.log(newNum)
+  newNum = Number(newNum);
+  return newNum;
 }
